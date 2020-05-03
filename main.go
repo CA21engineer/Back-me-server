d7bbce4a293825aac7e3ddc1b46ddb8fe7e064b9
Keisuke Mori <rjfs29kei@gmail.com>
2020-05-03 19:08:33 +0900

    modify DB connection setting for Cloud SQL

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"ca-zoooom/infrastructure"
+	"fmt"
+	"os"
 )
 
 func main() {
-	infrastructure.Router.Run(":8084")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8084"
+	}
+	infrastructure.Router.Run(fmt.Sprintf(":%s", port))
 }
