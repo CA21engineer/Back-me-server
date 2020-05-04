3f33f6f42c62b885b8b68fee7af8dc068c58753b
Keisuke Mori <rjfs29kei@gmail.com>
2020-05-04 13:20:21 +0900

    add count(*) interface to SqlHandler for counting table items

--- interfaces/db/sqlhandler.go
+++ interfaces/db/sqlhandler.go
@@ -1,6 +1,7 @@
 package db
 
 type SqlHandler interface {
+	SelectInt(query string, args ...interface{}) (int64, error)
 	SelectOne(holder interface{}, query string, args ...interface{}) error
 	Select(holder interface{}, query string, args ...interface{}) ([]interface{}, error)
 	Insert(list ...interface{}) error
