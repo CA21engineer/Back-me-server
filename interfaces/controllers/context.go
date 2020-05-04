5d168f198f94f967393addc43e90feb664868566
Keisuke Mori <rjfs29kei@gmail.com>
2020-05-04 13:18:53 +0900

    add query-string handling interface to Context

--- interfaces/controllers/context.go
+++ interfaces/controllers/context.go
@@ -2,6 +2,7 @@ package controllers
 
 type Context interface {
 	Param(string) string
+	DefaultQuery(key, defaultValue string) string
 	Bind(interface{}) error
 	Status(int)
 	JSON(int, interface{})
