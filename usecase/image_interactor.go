d408346c1087529ede092700d0d965739b24035e
nakao107107 <imakeanewstory@gmail.com>
2020-05-05 00:10:26 +0900

    s3に画像をuploadするsigned_urlを発行するend_pointの作成

--- usecase/image_interactor.go
+++ usecase/image_interactor.go
@@ -1,7 +1,14 @@
 package usecase
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/joho/godotenv"
 	"ca-zoooom/entity"
+	"time"
+	"os"
 )
 
 type ImageInteractor struct {
@@ -42,5 +49,36 @@ func (interactor *ImageInteractor) Add(image *entity.Image) (i entity.Image, err
 		return
 	}
 	interactor.StatusCode = 201
+	return
+}
+
+func (interactor *ImageInteractor) GetSignedUrl(rowImgName string) (url string, fileKey string, err error) {
+
+	//環境変数読み込み
+	err = godotenv.Load()
+
+	//ファイル名作成
+	fileKey = time.Now().Format("2006-01-02T15:04:05Z07:00") + rowImgName
+
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1"),
+		Credentials: credentials.NewStaticCredentialsFromCreds(credentials.Value{
+				AccessKeyID:     os.Getenv("ACCESS_KEY_ID"),
+				SecretAccessKey: os.Getenv("SECRET_ACCESS_KEY"),
+			}),
+		},
+	)
+
+	svc := s3.New(sess)
+	req, _ := svc.PutObjectRequest(&s3.PutObjectInput{
+		Bucket: aws.String("ca-home-hackathon"),
+		Key:    aws.String(fileKey),
+	})
+	url, err = req.Presign(15 * time.Minute)
+
+	if err != nil {
+		interactor.StatusCode = 500
+		return
+	}
 	return
 }
