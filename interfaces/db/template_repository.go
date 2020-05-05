0a49547f38910fde9235bd09513834cb2aae02a3
nakao107107 <imakeanewstory@gmail.com>
2020-05-05 16:49:51 +0900

    不要なコメント削除

--- interfaces/db/template_repository.go
+++ interfaces/db/template_repository.go
@@ -18,9 +18,6 @@ func (repo *TemplateRepository) Count() (count int, err error) {
 }
 
 func (repo *TemplateRepository) Get(limit int, offset int, keyword string) (templates entity.Templates, err error) {
-	//_, err = repo.Select(&templates,
-	//	"select * from templates LEFT JOIN template_tags ON templates.id = template_tags.template_id LEFT JOIN tags ON template_tags.tag_id = tags.id where tags.title = ? limit ? offset ?",
-	//	keyword, limit, offset)
 	keywordLike := "%"+keyword+"%"
 	_, err = repo.Select(&templates, "select templates.id, templates.uid, templates.background_url, templates.generated_sample_url, templates.created_at, templates.updated_at from templates " +
 		"LEFT JOIN template_tags ON templates.id = template_tags.template_id " +
